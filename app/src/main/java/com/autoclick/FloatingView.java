8fcd6e1dbde49d2092fc76abd07879485eade270
wangyu1920 <78335699+wangyu1920@users.noreply.github.com>
2021-11-21 21:42:04 +0800

    1121第三次

--- app/src/main/java/com/autoclick/FloatingView.java
+++ app/src/main/java/com/autoclick/FloatingView.java
@@ -1,7 +1,9 @@
 package com.autoclick;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Service;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
@@ -14,10 +16,12 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.genshinimpactmidiplayer.MainActivity;
 import com.example.genshinimpactmidiplayer.R;
+import com.hb.dialog.myDialog.MyAlertInputDialog;
 import com.leff.midi.MidiFile;
 import com.leff.midi.event.NoteOn;
 import com.leff.midi.examples.EventPrinter;
@@ -25,6 +29,7 @@ import com.leff.midi.examples.EventsCollection;
 import com.leff.midi.util.MidiProcessor;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class FloatingView extends Service implements View.OnClickListener {
@@ -134,6 +139,8 @@ public class FloatingView extends Service implements View.OnClickListener {
         stopButton.setOnClickListener(this);
         Button adjustButton = (Button) myFloatingView.findViewById(R.id.adjust);
         adjustButton.setOnClickListener(this);
+        Button chooseButton = (Button) myFloatingView.findViewById(R.id.choose);
+        chooseButton.setOnClickListener(this);
 
     }
 
@@ -156,7 +163,7 @@ public class FloatingView extends Service implements View.OnClickListener {
                     processor.reset();
                 }
                 //Get x0,y0,x1,y1 to construct the EventPrinter
-                //获取简单存储器  （存储是否删除服饰资源包的数据）
+                //获取简单存储器
                 SharedPreferences preferences = getSharedPreferences("p", MODE_PRIVATE);
                 int x0=preferences.getInt("x0", 600);
                 int y0=preferences.getInt("y0", 960);
@@ -208,6 +215,125 @@ public class FloatingView extends Service implements View.OnClickListener {
                     pr.edit().putInt("x1", location[0]).apply();
                     pr.edit().putInt("y1", location[1]).apply();
                     isFirstAdjust = true;
+                }
+                break;
+
+            case R.id.choose:
+                Toast.makeText(getApplicationContext(),"请输入文件名",Toast.LENGTH_SHORT).show();
+//                String path = getSharedPreferences("path", MODE_PRIVATE).getString("path", null);
+//                if (null == path) {break;}
+//                final MyAlertInputDialog myAlertInputDialog = new MyAlertInputDialog(this).builder()
+//                        .setTitle("请输入")
+//                        .setEditText("");
+//                myAlertInputDialog.setPositiveButton("确认", new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        String newPath=path+"/"+myAlertInputDialog.getResult();
+//                        if (newPath.charAt(newPath.length() - 1) != 'd') {
+//                            newPath += ".mid";
+//                        }
+//                        try {
+//                            File inputMid=new File(newPath);
+//                            midiToPlay = new MidiFile(inputMid);
+//                            ec = new EventsCollection(midiToPlay);
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                                Toast.makeText(getApplicationContext(),"错误",Toast.LENGTH_SHORT).show();
+//                            }
+//                        myAlertInputDialog.dismiss();
+//                    }
+//                }).setNegativeButton("取消", new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        myAlertInputDialog.dismiss();
+//                    }
+//                });
+//                myAlertInputDialog.show();
+                try {
+                    String path = getSharedPreferences("path", MODE_PRIVATE).getString("path", null);
+                    if (null != path) {
+                        int layout_parms;
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                            layout_parms = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+                        }
+                        else {
+                            layout_parms = WindowManager.LayoutParams.TYPE_PHONE;
+                        }
+                        //setting the layout parameters
+                        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                WindowManager.LayoutParams.WRAP_CONTENT,
+                                layout_parms,
+                                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN ,
+                                PixelFormat.TRANSLUCENT);
+                        //getting windows services and adding the floating view to it
+                        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                        View chooseMidiView = LayoutInflater.from(this).inflate(R.layout.floating_choose_midi, null);
+                        mWindowManager.addView(chooseMidiView, params);
+                        mWindowManager.removeView(myFloatingView);
+                        chooseMidiView.findViewById(R.id.thisIsAnotherID).setOnTouchListener(new View.OnTouchListener() {
+                            private int initialX;
+                            private int initialY;
+                            private float initialTouchX;
+                            private float initialTouchY;
+                            @Override
+                            public boolean onTouch(View v, MotionEvent event) {
+                                Log.d("TOUCH","THIS IS TOUCHED");
+                                switch (event.getAction()) {
+                                    case MotionEvent.ACTION_DOWN:
+                                        initialX = params.x;
+                                        initialY = params.y;
+                                        initialTouchX = event.getRawX();
+                                        initialTouchY = event.getRawY();
+                                        return true;
+
+                                    case MotionEvent.ACTION_UP:
+
+                                        return true;
+
+                                    case MotionEvent.ACTION_MOVE:
+                                        //this code is helping the widget to move around the screen with fingers
+                                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                                        mWindowManager.updateViewLayout(chooseMidiView, params);
+                                        return true;
+                                }
+                                return false;
+                            }
+                        });
+                        TextView textView = (TextView) chooseMidiView.findViewById(R.id.text_file_name);
+                        File[] list = new File(path).getCanonicalFile().listFiles();
+                        StringBuilder builder = new StringBuilder();
+                        assert list != null;
+                        for (File f : list) {
+                            String s=f.getName();
+                            System.out.println(s);
+                            if (s.endsWith(".mid"))
+                            builder.append(s).append("\n");
+                        }
+                        textView.setText(builder.toString());
+                        Button commitButton = (Button) chooseMidiView.findViewById(R.id.commit);
+                        commitButton.setOnClickListener(v1 -> {
+                            String newPath = path + "/" + ((EditText) chooseMidiView.findViewById(R.id.editText_MidiName)).getText().toString();
+                            if (newPath.charAt(newPath.length() - 1) != 'd') {
+                                newPath += ".mid";
+                            }
+                            File inputMid=new File(newPath);
+                            try {
+                                midiToPlay = new MidiFile(inputMid);
+                                ec = new EventsCollection(midiToPlay);
+                                Toast.makeText(getApplicationContext(),"成功找到文件:"+newPath,Toast.LENGTH_SHORT).show();
+                            } catch (IOException e) {
+                                Toast.makeText(getApplicationContext(),"没找到文件:"+newPath,Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                            }finally {
+                                mWindowManager.removeView(chooseMidiView);
+                                mWindowManager.addView(myFloatingView,params);
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),"错误",Toast.LENGTH_SHORT).show();
                 }
 
 
--- app/src/main/java/com/example/genshinimpactmidiplayer/MainActivity.java
+++ app/src/main/java/com/example/genshinimpactmidiplayer/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.genshinimpactmidiplayer;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -25,6 +34,7 @@ import com.leff.midi.MidiFile;
 import com.leff.midi.examples.EventsCollection;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -32,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 2084;
     MidiFile midiFile;
     EventsCollection ec;
+    String path="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,13 +57,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.stopFloat).setOnClickListener(this);
         //读midi文件按钮的初始化 以及 展示midi文件细节的TextView的初始化
         findViewById(R.id.button_ReadMidi).setOnClickListener(v -> {
+            verifyStoragePermissions();
             try {
-                verifyStoragePermissions(this);
-                File inputMid=new File(((EditText)findViewById(R.id.editText_MidiPath)).getText().toString());
-                midiFile = new MidiFile(inputMid);
-                ec = new EventsCollection(midiFile);
-                Toast.makeText(this, "读取成功", Toast.LENGTH_SHORT).show();
-                ((TextView)findViewById(R.id.textView_MidiDetails)).setText("共"+ec.getTotalValueNum()+"个音符；可以弹奏最多"+(int)(ec.getCanPlayRatio()*100)+"%个音符。低于80%将自动调整");
+                File inputMid;
+                if (((EditText) findViewById(R.id.editText_MidiPath)).getText().toString().length() <= 1) {
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+                    //intent.setType(“audio/*”); //选择音频
+                    //intent.setType(“video/*”); //选择视频 （mp4 3gp 是android支持的视频格式）
+                    //intent.setType(“video/*;image/*”);//同时选择视频和图片
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    startActivityForResult(intent, 11);
+                } else {
+                    inputMid = new File(((EditText) findViewById(R.id.editText_MidiPath)).getText().toString());
+                    midiFile = new MidiFile(inputMid);
+                    ec = new EventsCollection(midiFile);
+                    SharedPreferences preferences = getSharedPreferences("path", MODE_PRIVATE);
+                    preferences.edit().putString("path", new StringBuilder().append(((EditText) findViewById(R.id.editText_MidiPath)).getText().toString()).delete(
+                            ((EditText) findViewById(R.id.editText_MidiPath)).getText().toString().lastIndexOf("/"),
+                            ((EditText) findViewById(R.id.editText_MidiPath)).getText().toString().length()
+                    ).toString()).apply();
+                    ((TextView) findViewById(R.id.textView_MidiDetails)).setText("共" + ec.getTotalValueNum() + "个音符；可以弹奏最多" + (int) (ec.getCanPlayRatio() * 100) + "%个音符。低于80%将自动调整");
+                    Toast.makeText(this, "读取成功", Toast.LENGTH_SHORT).show();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println(e);
@@ -60,12 +87,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
         });
-        //两个checkBox的初始化
+        //三个checkBox的初始化
         if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this))) {
             ((CheckBox)findViewById(R.id.checkbox_permission_window)).setChecked(true);
         }
         if(isAccessibilitySettingsOn(getApplicationContext())) {
             ((CheckBox)findViewById(R.id.checkbox_permission_ally)).setChecked(true);
+        }
+        if (PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+                "android.permission.WRITE_EXTERNAL_STORAGE")) {
+            ((CheckBox)findViewById(R.id.checkbox_permission_io)).setChecked(true);
         }
         ((CheckBox)findViewById(R.id.checkbox_permission_window)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -82,6 +113,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
                     startActivity(intent);
                 }
+            }
+        });
+        ((CheckBox)findViewById(R.id.checkbox_permission_io)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                verifyStoragePermissions();
             }
         });
         //对联系开发者文字做的监听器，跳转到我的QQ资料页
@@ -133,7 +170,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivityForResult(intent, SYSTEM_ALERT_WINDOW_PERMISSION);
     }
 
-    public static void verifyStoragePermissions(Activity activity) {
+    public void verifyStoragePermissions() {
         int REQUEST_EXTERNAL_STORAGE = 1;
         String[] PERMISSIONS_STORAGE = {
                 "android.permission.READ_EXTERNAL_STORAGE",
@@ -141,11 +178,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 "android.permission.MOUNT_UNMOUNT_FILESYSTEMS"};
         try {
             //检测是否有写的权限
-            int permission = ActivityCompat.checkSelfPermission(activity,
+            int permission = ActivityCompat.checkSelfPermission(this,
                     "android.permission.WRITE_EXTERNAL_STORAGE");
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 // 没有写的权限，去申请写的权限，会弹出对话框
-                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -153,11 +190,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         try {
             //检测是否有写的权限
-            int permission = ActivityCompat.checkSelfPermission(activity,
+            int permission = ActivityCompat.checkSelfPermission(this,
                     "android.permission.MOUNT_UNMOUNT_FILESYSTEMS");
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 // 没有写的权限，去申请写的权限，会弹出对话框
-                ActivityCompat.requestPermissions(activity, new String[]{"android.permission.MOUNT_UNMOUNT_FILESYSTEMS"},REQUEST_EXTERNAL_STORAGE);
+                ActivityCompat.requestPermissions(this, new String[]{"android.permission.MOUNT_UNMOUNT_FILESYSTEMS"},REQUEST_EXTERNAL_STORAGE);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -179,7 +216,133 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         } else if (v.getId() == R.id.stopFloat) {
             stopService(new Intent(MainActivity.this, FloatingView.class));
+
         }
 
+    }
+
+    @SuppressLint("SdCardPath")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 11) {
+            if (resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    String thePath = getPath(this, uri);
+                    if (thePath != null) {
+                        path=thePath.replace("/storage/emulated/0/","/sdcard/");
+                        System.out.println(path+"\n\n\n\n\n\n\n\n\n");
+                        ((EditText)findViewById(R.id.editText_MidiPath)).setText(path);
+                        try {
+                            midiFile = new MidiFile(new File(path));
+                            ec = new EventsCollection(midiFile);
+                            SharedPreferences preferences = getSharedPreferences("path", MODE_PRIVATE);
+                            preferences.edit().putString("path", new StringBuilder().append(path).delete(
+                                    path.lastIndexOf("/"),
+                                    path.length()
+                            ).toString()).apply();
+                            ((TextView) findViewById(R.id.textView_MidiDetails)).setText("共" + ec.getTotalValueNum() + "个音符；可以弹奏最多" + (int) (ec.getCanPlayRatio() * 100) + "%个音符。低于80%将自动调整");
+                            Toast.makeText(this, "读取成功", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Toast.makeText(this, "失败，请检查文件路径是否正确", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            }
+            Log.e("导入失败","");
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public String getDataColumn(Context context, Uri uri, String selection,
+                                String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    public boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
